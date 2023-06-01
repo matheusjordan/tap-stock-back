d873bc57ed13c8df691942c169829118c7f670b1
matheus jordan <matheusj1599@gmail.com>
2023-05-31 22:28:56 -0300

    Added dockerfile

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: 5432,
+    port: 5432
 });
 
 app.get('/stocks', async (req, res) => {
